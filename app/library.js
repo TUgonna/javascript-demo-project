ef49dd8c9c2b2feda2cf7e4c787cb0839f4ebc4d
TUgonna <moncateyes@yahoo.com>
2016-11-25 04:49:19 +0100

    Update library.js

--- app/library.js
+++ app/library.js
@@ -1,20 +1,20 @@
 module.exports={
-	computeAverage: function(num1, num2, num3) {
-		return (num1+num2+num3)/3;
-	},
-	computeFactorial: function(num) {
-		var fact=1;
-		for (var i=num; i>0; i--)
-		{
-		fact*=i;
-		}
-		return fact;
-	},
-	convertTempCtoF: function(numb) {
-		return (9/5*numb)+32;
-	},
-	convertTempFtoC: function(numb) {
-		return 5/9*(numb-32);
-	}
+  computeAverage: function(num1, num2, num3) {
+    return (num1+num2+num3)/3;
+  },
+  computeFactorial: function(num) {
+    var fact=1;
+    for (var i=num; i>0; i--)
+	  {
+	    fact*=i;
+	  }
+	return fact;
+  },
+  convertTempCtoF: function(numb) {
+    return (9/5*numb)+32;
+  },
+  convertTempFtoC: function(numb) {
+    return 5/9*(numb-32);
+  }
 
 }
